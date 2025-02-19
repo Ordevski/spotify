fb1fd82d1162772472aa14a5eb795cd74a98b4a1
unknown <ordevskidd@gmail.com>
2025-02-19 04:48:02 +0100

    Album and Song APIs

--- spotify-backend/src/controllers/albumController.js
+++ spotify-backend/src/controllers/albumController.js
@@ -1,13 +1,89 @@
-const addAlbum = async (req, res) => {
+import { v2 as cloudinary } from 'cloudinary';
+import Album from '../models/Album.js';
+import Song from '../models/Song.js';
 
+const addAlbum = async (req, res) => {
+    try {
+        const { name, desc, bgColor } = req.body;
+        const imageFile = req.file;
+
+        if(!name || !desc || !bgColor || !imageFile) {
+            return res.status(500).json({
+                success: false,
+                message: "Please provide all fields."
+            });
+        }
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+
+        const albumData = { name, desc, bgColor, image: imageUpload.secure_url };
+        const album = Album(albumData);
+
+        await album.save();
+        res.status(201).json({
+            success: true,
+            message: "Album sucessfully saved."
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ 
+            success: false, 
+            message: "Error in ADD ALBUM API" 
+        });
+    }
 }
 
 const listAlbum = async (req, res) => {
+    try {
+        const allAlbums = await Album.find({});
 
+        if(!allAlbums) {
+            return res.status(404).json({
+                success: false,
+                message: "No albums found.",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Albums retrieved successfully.",
+            allAlbums
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error in LIST ALBUMS API.",
+        });
+    }
 }
 
 const removeAlbum = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if(!id){
+            return res.status(404).json({
+                success: false,
+                message: "Please provide an ID."
+            });
+        }
 
+        const album = await Album.findById({ _id: id });
+        const albumSongs = await Song.find({ album: album.name });
+
+        albumSongs.map(async (item) => {
+            await Song.findByIdAndDelete({ _id: item._id });
+        });
+        await Album.findByIdAndDelete({ _id: id });
+
+        res.status(200).json({ 
+            success: true, 
+            message: "Album removed successfully." });
+    } catch (error) {
+        console.log('Failed at removeAlbum, ', error);
+        res.status(400).json({ 
+            success: false, 
+            message: "Error in ALBUM REMOVE API"});
+    }
 }
 
 export { addAlbum, listAlbum, removeAlbum }
--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,13 +1,88 @@
-const addSong = async (req, res) => {
+import { v2 as cloudinary } from 'cloudinary';
+import Song from '../models/Song.js';
 
+const addSong = async (req, res) => {
+    try {
+        const { name, desc, album } = req.body;
+        const audioFile = req.files.audio[0];
+        const imageFile = req.files.audio[0];
+
+        if(!name || !desc || !album || !audioFile || !imageFile) {
+            return res.status(500).json({
+                success: false,
+                message: "Please provide all fields."
+            });
+        }
+
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video"});
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"});
+        const duration = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(audioUpload.duration % 60)}`;
+
+        const songData = { name, desc, album, duration, image: imageUpload.secure_url, file: audioUpload.secure_url };
+        const song = Song(songData);
+
+        await song.save();
+
+        res.status(201).json({
+            success: true,
+            message: "Song successfully saved.",
+            song
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error in ADD SONG API.",
+        });
+    }
 }
 
 const listSong = async (req, res) => {
+    try {
+        const allSongs = await Song.find({});
+        if(!allSongs) {
+            return res.status(404).json({
+                success: false,
+                message: "No songs found.",
+            });
+        }
 
+        res.status(200).json({
+            success: true,
+            message: "Songs retrieved successfully.",
+            allSongs
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error in LIST SONGS API.",
+        });
+    }
 }
 
 const removeSong = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if(!id){
+            return res.status(404).json({
+                success: false,
+                message: "Please provide an ID."
+            });
+        }
 
+        await Song.findByIdAndDelete({ _id: id });
+        res.status(200).json({
+            success: true,
+            message: "Song successfully deleted."
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error in REMOVE SONG API.",
+        });
+    }
 }
 
 export { addSong, listSong, removeSong }
